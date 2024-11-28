33306acc452c562d2a8764dd2fc368e302367355
Rafael Santana <rafaelsantana7213@gmail.com>
2024-11-27 22:42:13 -0300

    feat: add libs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,24 @@
 import { Stack } from 'expo-router'
+import '@/global.css'
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider'
 const { Screen } = Stack
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
 export default function Layout() {
+  const queryClient = new QueryClient()
+
   return (
-    <Stack screenOptions={{}}>
-      <Screen name="index" />
-      <Screen name="sing-up" />
-      <Screen
-        name="(auth)" options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <GluestackUIProvider mode="light">
+        <Stack>
+          <Screen name="index" />
+          <Screen
+            name="(auth)" options={{
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </GluestackUIProvider>
+    </QueryClientProvider>
   )
 }
